a37266cba9a0519c9810111f92963dff917a8187
domsmith94 <domsmith94@gmail.com>
2016-07-28 08:40:00 +0100

    added enum for status and shipping

--- src/com/sky/entities/Order.java
+++ src/com/sky/entities/Order.java
@@ -4,12 +4,21 @@ import java.util.Date;
 
 public class Order {
 	
+	public enum Status {
+		COMPLETE, DELIVERY, RECIEVED, PROCESSING
+		
+	}
+	
+	public enum Shipping {
+		STANDARD, PREMIUM
+	}
+	
 	private int orderId;
 	private Customer orderedBy;
 	private OrderLine[] orderLines; // OrderLine will contain product and quant
 	private Double totalCost;
-	private Enum status; // need to look how to do Enums complete, cancelled, refund etc 
-	private Enum shippingType;
+	private Status status; // need to look how to do Enums complete, cancelled, refund etc 
+	private Shipping shippingType;
 	private Date orderTime;
 	private Address deliveryAddress;
 	
