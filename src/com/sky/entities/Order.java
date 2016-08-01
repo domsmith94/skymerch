51cb0b6455a3b60f44b920846db99c144b2dd99d
domsmith94 <domsmith94@gmail.com>
2016-08-01 17:03:34 +0100

    created getters and setters for Order and Product

--- src/com/sky/entities/Order.java
+++ src/com/sky/entities/Order.java
@@ -1,6 +1,8 @@
 package com.sky.entities;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import com.sky.enums.Shipping;
 import com.sky.enums.Status;
@@ -9,7 +11,7 @@ public class Order {
 	
 	private int orderId;
 	private Customer orderedBy;
-	private OrderLine[] orderLines; // OrderLine will contain product and quant
+	private List<OrderLine> orderLines; // OrderLine will contain product and quant
 	private Double totalCost;
 	private Status status; // need to look how to do Enums complete, cancelled, refund etc 
 	private Shipping shippingType;
@@ -19,6 +21,40 @@ public class Order {
 	public Order(){
 		
 	}
+
+	public int getOrderId() { return orderId; }
+
+	public void setOrderId(int orderId) { this.orderId = orderId; }
+
+	public Customer getOrderedBy() { return orderedBy; }
+
+	public void setOrderedBy(Customer orderedBy) { this.orderedBy = orderedBy; }
+
+	public List<OrderLine> getOrderLines() { return orderLines; }
+
+	public void setOrderLines(List<OrderLine> orderLines) { this.orderLines = orderLines; }
+
+	public Double getTotalCost() { return totalCost; }
+
+	public void setTotalCost(Double totalCost) { this.totalCost = totalCost; }
+
+	public Status getStatus() { return status; }
+
+	public void setStatus(Status status) { this.status = status; }
+
+	public Shipping getShippingType() { return shippingType; }
+
+	public void setShippingType(Shipping shippingType) { this.shippingType = shippingType; }
+
+	public Date getOrderTime() { return orderTime; }
+
+	public void setOrderTime(Date orderTime) { this.orderTime = orderTime; }
+
+	public Address getDeliveryAddress() { return deliveryAddress; }
+
+	public void setDeliveryAddress(Address deliveryAddress) { this.deliveryAddress = deliveryAddress; }
+	
+	
 	
 	
 	
