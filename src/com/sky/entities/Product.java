e00e8b5c34ca13703effb63e999666163c5974b7
domsmith94 <domsmith94@gmail.com>
2016-08-01 17:11:49 +0100

    updated Product keywords to list array

--- src/com/sky/entities/Product.java
+++ src/com/sky/entities/Product.java
@@ -1,5 +1,7 @@
 package com.sky.entities;
 
+import java.util.List;
+
 public class Product {
 	
 	private int prodId;
@@ -8,13 +10,69 @@ public class Product {
 	private String prodName;
 	private String prodDesc;
 	private Double price;
-	private String[] keywords; //ArrayList :)
+	private List<String> keywords; //ArrayList :)
 	private String location;
 	private Enum category; // enum to limit options
 	
 	
+	public Product(){
+		
+	}
+
+
+	public int getProdId() { return prodId; }
+
+
+	public void setProdId(int prodId) { this.prodId = prodId; }
+
+
+	public int getStockLevel() { return stockLevel; }
+
+
+	public void setStockLevel(int stockLevel) { this.stockLevel = stockLevel; }
+
+
+	public int getReorderLevel() { return reorderLevel; }
+
+
+	public void setReorderLevel(int reorderLevel) { this.reorderLevel = reorderLevel; }
+
+
+	public String getProdName() { return prodName; }
+
+
+	public void setProdName(String prodName) { this.prodName = prodName; }
+
+
+	public String getProdDesc() { return prodDesc; }
+
+
+	public void setProdDesc(String prodDesc) { this.prodDesc = prodDesc; }
+
+
+	public Double getPrice() { return price; }
+
+
+	public void setPrice(Double price) { this.price = price; }
+
+
+	public List<String> getKeywords() { return keywords; }
+
+
+	public void setKeywords(List<String> keywords) { this.keywords = keywords; }
+
+
+	public String getLocation() { return location; }
+
+
+	public void setLocation(String location) { this.location = location; }
+
+
+	public Enum getCategory() { return category; }
+
+
+	public void setCategory(Enum category) { this.category = category; }
 	
 	
-	// 
 
 }
