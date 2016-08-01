db8a694b7015aace87249ce12d99a76f09eeea98
domsmith94 <domsmith94@gmail.com>
2016-08-01 16:38:43 +0100

    Added getters and setters for Address

--- src/com/sky/entities/Address.java
+++ src/com/sky/entities/Address.java
@@ -13,12 +13,29 @@ public class Address {
 		
 	}
 	
-	public String getHouseNameNum(){
-		return this.houseNameNum;
-	}
+	public String getHouseNameNum() { return this.houseNameNum; }
 	
+	public void setHouseNameNum(String houseNameNum) { this.houseNameNum = houseNameNum; }
 	
+	public String getAddressLineOne() { return this.addressLineOne; }
 	
+	public void setAddressLineOne(String addressLineOne) { this.addressLineOne = addressLineOne; }
+
+	public String getAddressLineTwo() { return addressLineTwo; }
+
+	public void setAddressLineTwo(String addressLineTwo) { this.addressLineTwo = addressLineTwo; }
+
+	public String getRegion() { return region; }
+
+	public void setRegion(String region) { this.region = region; }
+
+	public String getPostcode() { return postcode; }
+
+	public void setPostcode(String postcode) { this.postcode = postcode; }
+
+	public String getCountry() { return country; }
+
+	public void setCountry(String country) { this.country = country; }
 	
 
 }
