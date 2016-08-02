e3d0ba6d8db4ceb0ca65df941be31402929cea9e
ligforbes <ligforbes@gmail.com>
2016-08-02 21:34:50 +0100

    added test Customer instance with full details

--- src/skymerch/tests/DAOTest.java
+++ src/skymerch/tests/DAOTest.java
@@ -1,5 +1,7 @@
 package skymerch.tests;
 
+import skymerch.entities.Address;
+import skymerch.enums.*;
 import skymerch.entities.Customer;
 import skymerch.entities.CustomerDAO;
 
@@ -7,6 +9,21 @@ public class DAOTest {
 	
 	public static void main(String[] args) {
 		CustomerDAO dao = new CustomerDAO();
+		Customer bel = new Customer();
+		bel.setFirstName("Isobel");
+		bel.setLastName("Forbes");
+		bel.setTitle(Title.MS);
+		bel.setEmail("belbel@cutemail.com");
+		bel.setPassword("iloveD4da");
+		Address belsAddr = new Address();
+		belsAddr.setHouseNameNum("2/4");
+		belsAddr.setAddressLineOne("Great Michael Rise");
+		belsAddr.setAddressLineTwo("Newhaven");
+		belsAddr.setRegion("Edinburgh");
+		belsAddr.setCountry("Scotland");
+		belsAddr.setPostcode("EH6 4JB");
+		bel.setAddress(belsAddr);
+		dao.addCustomer(bel);
 		//dao.addCustomer(new Customer());
 		Customer customer = dao.findByEmail("adam.morrison@sky.uk");
 		System.out.println(customer.getFirstName());
