35faf0e9d079e96f684001460b08dddea48baa36
ligforbes <ligforbes@gmail.com>
2016-08-02 12:04:24 +0100

    draft CustomerValidator class

---
+++ src/skymerch/entities/CustomerValidator.java
@@ -0,0 +1,25 @@
+package skymerch.entities;
+
+public class CustomerValidator {
+	
+	public static boolean isValid(Customer c){
+		
+		boolean valid = false;
+		if (c.getFirstName().length()>0 && c.getFirstName().length()<=30){
+			if (c.getLastName().length()>0 && c.getFirstName().length()<=30){
+				if(c.getEmail().contains("@")){
+				valid = true;
+		}
+				else {
+					valid = false;
+				}
+		
+		
+	}
+		}
+		return valid;
+	}
+}
+		
+	
+	
