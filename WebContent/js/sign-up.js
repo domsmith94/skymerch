2b8c154f2e562ca74cf4028ffd48e10437d03933
alicetuck <alice.tuck@sky.uk>
2016-08-10 11:50:02 +0100

    Worked on sign up jquery

--- WebContent/js/sign-up.js
+++ WebContent/js/sign-up.js
@@ -2,8 +2,16 @@ $(document).ready(function() {
 	$("#btnSubmit").click(function(){
 		var title = $("#title").val();
 		var firstName = $("#firstName").val();
-
-		alert(firstName);
+		var lastName = $("#lastName").val();
+		var email = $("#email").val();
+		var mobileNumber = $("#mobileNumber").val();
+		var password = $("#password").val();
+		var confirmPassword = $("#confirmPassword").val();
+		
+		if (password != confirmPassword) {
+			alert("Passwords don't match");
+		}
+		alert(firstName + " is" );
 	});
 
 });
