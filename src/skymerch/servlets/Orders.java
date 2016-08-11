89d7ac702ce591813425da2f26665f6be15ceb69
domsmith94 <domsmith94@gmail.com>
2016-08-11 16:38:38 +0100

    added orders servlet

---
+++ src/skymerch/servlets/Orders.java
@@ -0,0 +1,68 @@
+package skymerch.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Orders
+ */
+@WebServlet("/orders")
+public class Orders extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Orders() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = null;
+		String urlPattern = request.getServletPath();
+		HttpSession session = request.getSession();
+		
+		try {
+			boolean auth;
+			
+			if (null == session.getAttribute("auth")){
+				auth = false;
+			} else {
+				auth = true;
+			}
+						
+			if (auth){
+				rd = request.getRequestDispatcher("/order_history.html");
+				rd.forward(request, response);
+				
+			} else {
+				response.sendRedirect("/skymerch");
+			}
+			
+		} catch (Exception e) {
+			
+		}
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
