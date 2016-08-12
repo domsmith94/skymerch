689477efe55710f0e0d21231b1a603313d7a1426
ligforbes <ligforbes@gmail.com>
2016-08-12 15:42:13 +0100

    started order history servlet

---
+++ src/skymerch/servlets/OrderHistoryServlet.java
@@ -0,0 +1,58 @@
+package skymerch.servlets;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.SortedSet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import skymerch.dao.ProductDAO;
+import skymerch.entities.Product;
+
+/**
+ * Servlet implementation class OrderHistoryServlet
+ */
+@WebServlet("/order_history")
+public class OrderHistoryServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public OrderHistoryServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = null;
+		HttpSession session = request.getSession();
+		
+		OrderDAO odao = new OrderDAO();
+		SortedSet<Product> orderHist = odao.getOrdersByCustId(int)
+		session.setAttribute("orderHistory", orderHist);
+		
+		rd = this.getServletContext().getRequestDispatcher("/order_history.jsp");
+		rd.forward(request, response);		
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
